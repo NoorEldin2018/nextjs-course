a29b9fe11f88b21944c94aad94f3fda004be3c6f
Noor Eldin Essam <n.essam@orangestudio.com>
2023-05-22 20:02:39 +0300

    nextjs-course

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  // env: {
+  //   APP_DEV: "http://localhost:3000",
+  //   APP_PROD: "",
+  //   MONGO_URI: "mongodb+srv://noormessi1054:M4EqJ3bxNTK0ylpl@cluster0.0ruacg5.mongodb.net/products-app?retryWrites=true&w=majority"
+  // },
+  images: {
+    domains: ["images.pexels.com"],
+  },
 }
 
 module.exports = nextConfig
